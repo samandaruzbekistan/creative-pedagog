d5d6523fdddca4deefde1e88af61c5cff06f6397
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2023-11-16 00:43:39 +0500

    added admin migrations

---
+++ database/migrations/2023_11_15_185904_create_admins_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string("fullname");
+            $table->string("username");
+            $table->string("password");
+            $table->timestamps();
+        });
+
+        \Illuminate\Support\Facades\DB::table("admins")
+            ->insert([
+                'fullname' => "Samandar",
+                'username' => "admin",
+                'password' => "admin",
+                'created_at' => \Carbon\Carbon::now()->toDateString(),
+                'updated_at' => \Carbon\Carbon::now()->toDateString(),
+            ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        // Matches The "/admin/users" URL
+    });
 });
