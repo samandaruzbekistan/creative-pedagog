668e93f3e1ab52b5fe51f46b25fd451c95378b4d
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2023-11-22 03:13:46 +0500

    bux fixed

--- database/migrations/2023_11_15_185904_create_admins_table.php
+++ database/migrations/2023_11_15_185904_create_admins_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+
     /**
      * Run the migrations.
      *
@@ -30,6 +31,7 @@ return new class extends Migration
                 'updated_at' => \Carbon\Carbon::now()->toDateString(),
             ]);
 //        comment
+
     }
 
     /**
