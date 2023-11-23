9f64e83233e26e34d3612a7eeb5d5de63d4db15c
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2023-11-23 23:37:24 +0500

    user features created bug fixed 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,7 @@ class UserController extends Controller
     {
     }
 
+
     public function school_books(){
         $books = $this->schoolBooksRepository->getAllBooks();
         return view('user.school', ['']);
